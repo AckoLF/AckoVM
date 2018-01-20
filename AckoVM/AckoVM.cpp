5313d9158365974f530fedf5a7fa1b07ea4ad246
Aleksandar Ivanović <acko@lotusflare.com>
2018-01-20 18:51:55 +0100

    Added external library for handling Partitions

--- AckoVM/AckoVM.cpp
+++ AckoVM/AckoVM.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "part.h"
+
+#pragma comment(lib, "part.lib")
+
 using namespace std;
 
 int main(void) {
   cout << "Hello, World!" << endl;
+
+  auto p1 = new Partition("p1.ini");
+  cout << p1->getNumOfClusters() << endl;
+
   return 0;
 }
